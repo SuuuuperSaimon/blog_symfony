f3b9dbe73d8f31c55796799d887a8e1af7542f77
Yaroslav Sakhno <yar.sakhno@gmail.com>
2020-08-13 18:16:51 +0300

    changed path

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -54,7 +54,7 @@ class RegistrationController extends AbstractController
                     ->from(new Address('support@philosofy.com', 'Philosofy'))
                     ->to($user->getEmail())
                     ->subject('Please Confirm your Email')
-                    ->htmlTemplate('registration/confirmation_email.html.twig')
+                    ->htmlTemplate('back/email/confirmation_email.html.twig')
             );
             // do anything else you need here, like send an email
 
